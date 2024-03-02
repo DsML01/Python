9fe86a3e0d3326fac040e556cec09816740c0fbf
davi <davimelovas61@gmail.com>
2024-03-02 00:19:35 -0300

    Made my first main function in Python

--- functions.py
+++ functions.py
@@ -1,7 +1,27 @@
-def hello(name):
+#u can define the main function above everything and the other functions
+#under it and when use the app, call the main function at the very bottom line
+#in order to be able to use every single part of your code
+
+def main():
+    x = int(input("X = "))
+    y = int(input("Y = "))
+
+    print(sum(x, y))
+
+
+
+
+#if you call the function without passing arguments, it's gonna use " name = "User" " by default
+def hello(name = "user"):
     print(f"Hello, {name}")
 
-name = input("What's you name? ")
+#name = input("What's you name? ")
 
-hello(name)
+#hello()
+    
+def sum(x, y):
+    z = x + y
+    return z
+
+main()
 
