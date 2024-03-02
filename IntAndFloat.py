770d143bf07dc319f70a825595feadfcd5a64ff5
davi <davimelovas61@gmail.com>
2024-03-02 00:04:15 -0300

    Commit

---
+++ IntAndFloat.py
@@ -0,0 +1,28 @@
+
+a = 1 + 1
+
+b = 3
+
+print(f"{a + b}")
+
+x = int (input("What's x? "))
+y = int (input("What's y? "))
+
+z = int (x) + int (y)
+
+print(x + y)
+
+x = float (input("What's x? "))
+y = float (input("What's y? "))
+
+#it does the same thing as %.2f does
+z = round(x + y, 2)
+
+print(z)
+
+x = 1.33
+y = 2
+z = x / y
+
+#it does the same thing as %.2f does
+print(f"{z:.2f}")
