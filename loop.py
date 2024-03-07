aa8181636c64d98ca90e217e88468bc37ef8d630
davi <davimelovas61@gmail.com>
2024-03-07 11:26:40 -0300

    Added loop file

---
+++ loop.py
@@ -0,0 +1,26 @@
+"""
+i = 2
+
+while i != 0:
+    print("Travazap 1")
+    i -= 1
+
+while i <= 2:
+    print("Travazap 2")
+    i += 1
+
+for i in [0, 1, 2]:
+    print("Travazap")
+
+    n = int(3)
+
+for i in range(n):
+    print(f"Travazap {i}")
+
+#it does print my string 3 times with \n and in the end dont print 2 \n
+print("Meow\n" * 3, end = "")    
+
+"""
+
+
+
